de31fe585a1d288d62ed86d73e06a96b9e55d9d9
Tulasi-Reddy29 <tulasisasanoolareddy@gmail.com>
2025-01-30 09:37:14 +0530

    Add files via upload

---
+++ countvowels.java
@@ -0,0 +1,15 @@
+public class countvowels {
+    public static void main(String[] args) {
+        int count = 0;
+        String str = "My name is Rishitha";
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+
+                count++;
+            }
+        }
+        System.out.println("THE NO. OF VOWELS:" + count);
+    }
+}
